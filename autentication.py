4d261e8360b91f51e6a52d2ee6134d2b2bb28817
Moises Alfredo Gutiérrez Haad <ghmoises97@gmail.com>
2026-03-03 09:51:17 -0500

    Correccion_final

--- autentication.py
+++ autentication.py
@@ -4,4 +4,3 @@ print ("=TechBank Riwi digital=")
 print ("=======================")
 print ("bienvenido", n, "Por favor ingrese el pin: ")
 usuario_autenticacion = "David"
-usuario = input ("Pin: ")
