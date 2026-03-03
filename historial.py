63d82aa70c683d07a04f4e22c3f10c19f4d3698f
1juandcano <1juandcano@gmail.com>
2026-03-03 10:16:46 -0500

    Quinto commit

--- historial.py
+++ historial.py
@@ -1,3 +1,16 @@
+historial = []
+
+def registrar(nombre, tipo, monto, saldo):
+    if monto is None:
+        historial.append(f"{nombre} - {tipo} - saldo: ${saldo}")
+    else:
+        historial.append(f"{nombre} - {tipo} - monto: ${monto} - saldo: ${saldo}")
+
+def mostrar():
+    if len(historial) == 0:
+        print("no hay operaciones registradas.")
+        return
+
     print("\n===== historial =====")
     for operacion in historial:
         print(operacion)
