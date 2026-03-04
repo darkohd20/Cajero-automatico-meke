f66066cda5e3bb4fc73e09bb0249b5a4ebc8788f
estebanp26 <estejps2607@gmail.com>
2026-03-04 09:23:07 -0500

    codigo prim

--- reglas.py
+++ reglas.py
@@ -1,7 +1,8 @@
 if monto<=0:
-    print("Error: El monto debe ser mayor a 0")
-    return False
 
+    print("Error: El monto debe ser mayor a 0")
+    
 if monto > saldo:
    print("Error: Fondos insuficientes")
-   return False
+
+   
