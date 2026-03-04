7e42d50f42df2dcbcac66dae9415bf9107d037b6
Camila Marrugo <marrugocamille@gmail.com>
2026-03-04 08:21:19 -0500

    Actualización lista para IMPORT

--- control-intentos.py
+++ control-intentos.py
@@ -1,12 +1,15 @@
+usuario_autenticacion = "David"
 def control_intentos():
 
     for i in range(3):
-        pin = input("Ingrese PIN: ")
+        
 
         if pin.lower() == usuario_autenticacion.lower():
             print("PIN Correcto.")
             break
         else:
             print(f"Error, PIN incorrecto. Intento: {i + 1}")
+            pin = input("Ingrese PIN: ")
     else:
         print("No tiene más intentos.")
+        return False
