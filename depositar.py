26eb57092f41aac5c991973ba379f5fb29c63832
Juan Cañas <juanjimenez025@gmail.com>
2026-03-05 17:58:10 -0500

    Convertiendo a función

--- depositar.py
+++ depositar.py
@@ -1,4 +1,5 @@
-monto = int(input("Ingrese monto a depositar: "))
-saldo += monto
-print("Depósito exitoso")
-print(f"Nuevo saldo: {saldo}")
+def deposito(saldo, monto)
+    saldo += monto
+    print("Depósito exitoso")
+    print(f"Nuevo saldo: {saldo}")
+    return saldo
