8e8675cb7297f8d06fa03cdeaef5fc6dafc8e0ad
Moises Alfredo Gutiérrez Haad <ghmoises97@gmail.com>
2026-03-05 07:26:32 -0500

    Correccion_finalv2

--- autentication.py
+++ autentication.py
@@ -1,6 +1,19 @@
+form control-intentos.py inmport control_intentos
+form menu.py import menu
+
 print("Fecha:", datetime.now().strftime("%I:%M:%S %p"))
 print ("=======================")
 print ("=TechBank Riwi digital=")
 print ("=======================")
-print ("bienvenido", n, "Por favor ingrese el pin: ")
-usuario_autenticacion = "David"
+
+def autenticar():
+    usuario_autenticacion = "David"
+    pin =  input ("ingrese pin: ")
+
+    if pin.lower() == usuario_autenticacion.lower():
+        return True
+    else:
+        return control-intentos(pin)
+ingreso=autenticar()
+if ingreso:
+    return menu()
