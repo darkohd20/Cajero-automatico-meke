36d73843941e14264065310c60da1076e45630ba
darkohd20 <cristiandelahoz2028@gmail.com>
2026-03-05 10:11:58 -0500

    menu final

--- menu.py
+++ menu.py
@@ -1,38 +1,48 @@
+from ausgfuasdif import consultar_saldo
 def menu():
     print("\n--- Menu Ñekli ---")
-    print("1. Depositar")
-    print("2. Retirar")
-    print("3. Ver saldo")
+    print("1. Consultar saldo")
+    print("2. Retirar dinero")
+    print("3. Depositar dinero")
     print("4. Salir")
 
+def opciones():
+    while True:
+        try:
+            operaciones = int(input("¿Cuántas operaciones desea realizar?: "))
 
-while True:
-    try:
-        operaciones = int(input("Cuántas operaciones desea realizar?: "))
-        
+            if operaciones <= 0:
+                print("Debe ingresar un número válido")
+            else:
+                return operaciones
 
-        if operaciones <= 0:
-            print("Debe ingresar un número válido")
-        else:
-            break
+        except ValueError:
+            print("Eso no es un número, inténtelo otra vez")
 
-    except ValueError:
-        print("Eso no es un número, inténtelo otra vez")
 
-for i in range(operaciones):
-    menu()
-    opcion = input("Seleccione una opción: ")
-    if opcion == "1":
-        print("Ejecutando opción uno...")
+def real(operaciones):
+    for i in range(operaciones):
 
-    elif opcion == "2":
-        print("Ejecutando opción dos...")
+        menu()
 
-    elif opcion == "3":
-        print("Ejecutando opción tres...")
+        try:
+            opcion = int(input("Seleccione una opción: "))
 
-    elif opcion == "4":
-        print("Saliendo de Ñekli ;)")
-        break
-    else:
-        print("Opción no válida, intente de nuevo.")
+            if opcion == 1:
+                consultar_saldo()
+
+            elif opcion == 2:
+                print("Ejecutando opción dos...")
+
+            elif opcion == 3:
+                print("Ejecutando opción tres...")
+
+            elif opcion == 4:
+                print("Saliendo de Ñekli ;)")
+                break
+
+            else:
+                print("Opción no válida")
+
+        except ValueError:
+            print("Debe ingresar un número")
