3ac8901543d6b066ca5e8f3808a1fd471810fd02
onomauri1 <onofremauro0@gmail.com>
2026-03-05 09:16:26 -0500

    v6

--- c1-feature-retiro.py
+++ c1-feature-retiro.py
@@ -1,10 +1,5 @@
-
-monto = int(input("Ingrese el monto a retirar: "))
-
-            elif monto > saldo:
-                print("Saldo insuficiente")
-            else:
-                saldo -= monto
-                print("Retiro exitoso")
-                print(f"Nuevo saldo: {saldo}")
-                break
+def retirar(saldo, monto):
+    saldo -= monto
+    print("Retiro exitoso")
+    print(f"Nuevo saldo: {saldo}")
+    return saldo
