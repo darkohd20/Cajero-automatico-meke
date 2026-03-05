17758c5b4c9e2a7ede80d2228e153ce0691c6306
estebanp26 <estejps2607@gmail.com>
2026-03-05 18:29:28 -0500

    cambio de rama final

--- reglas.py
+++ reglas.py
@@ -1,11 +1,12 @@
-def validar_reglas(monto, saldo):
+def validar_reglas(tipo_de_transaccion, monto, saldo):
 
     if monto <= 0:
         print("El monto debe ser mayor a 0")
         return False
 
-    elif monto > saldo:
-        print("Saldo insuficiente")
-        return False
+    if (tipo_de_transaccion == "retiro"):
+        if monto > saldo:
+            print("Saldo insuficiente")
+            return False
 
     return True
