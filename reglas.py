5bbdeb32574fd5dfc93c1cff42d6bcad8389ba2a
estebanp26 <estejps2607@gmail.com>
2026-03-05 07:05:00 -0500

    corregido

--- reglas.py
+++ reglas.py
@@ -1,7 +1,6 @@
-if monto<=0:            
+if monto<=0:
     print("Error: El monto debe ser mayor a 0")
-    return False
 
 if monto > saldo:
    print("Error: Fondos insuficientes")
-   return False
+   
