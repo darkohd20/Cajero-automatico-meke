f215a64ce2b4e1028da714d2b7f65a1acab81f58
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-05 21:15:55 -0500

    aclaracion de la funcion de tope fun.auxiliar.py

--- fun.auxiliar.py
+++ fun.auxiliar.py
@@ -1,6 +1,7 @@
 #funcion auxilar
 def variables_del_tope_de_retiros(retiros_realizados, limite_de_retiros):
     if retiros_realizados >= limite_de_retiros:
-        print("\n[!] Lo sentimos, has superado el tope de retiros permitido (Máximo 3).")
-        return False
-    return True
+       # print("\n[!] Lo sentimos, has superado el tope de retiros permitido (Máximo 3).")
+       # return False
+   # return True
+#La función que está definida como variables_del_tope_de_retiros se ha simplificado para que, a la hora de importar el código, la lógica no se vea afectada por el print, pasando este a ser un comentario.
