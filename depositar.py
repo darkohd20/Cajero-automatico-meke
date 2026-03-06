3a6aff6cc3a748a8f3ff51081240aae85bcfbe25
Juan Cañas <juanjimenez025@gmail.com>
2026-03-05 19:02:31 -0500

    Cambio de rama final

--- depositar.py
+++ depositar.py
@@ -1,5 +1,18 @@
-def deposito(saldo, monto)
-    saldo += monto
-    print("Depósito exitoso")
-    print(f"Nuevo saldo: {saldo}")
-    return saldo
+from saldo import actualizar_saldo
+from historial import registrar_historial
+from config import obtener_nombre
+from reglas import validar_reglas
+from validacion_numeral import validar_numero
+
+global nombre
+
+def depositar(saldo):
+    monto = int(input("Ingrese monto a depositar: "))
+
+    if (validar_numero(monto)):
+        if (validar_reglas("deposito", monto, saldo)):
+            saldo = saldo + monto
+            registrar_historial(obtener_nombre(), "Deposito", monto, saldo)
+            actualizar_saldo(saldo)
+            print("Depósito exitoso")
+            print(f"Nuevo saldo: {saldo}")
